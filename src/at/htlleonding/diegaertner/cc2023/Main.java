6e23c20c5d462b21c94c8b63c8a31cb4b260e42c
LaurinPoelderl <laurin.poelderl@gmail.com>
2023-10-20 11:43:49 +0200

    lvl 2 finished

--- src/at/htlleonding/diegaertner/cc2023/Main.java
+++ src/at/htlleonding/diegaertner/cc2023/Main.java
@@ -1,13 +1,14 @@
 package at.htlleonding.diegaertner.cc2023;
 
 import at.htlleonding.diegaertner.cc2023.levels.Level1;
+import at.htlleonding.diegaertner.cc2023.levels.Level2;
 import at.htlleonding.diegaertner.cc2023.util.FileUtils;
 
 public class Main {
 	public static void main(String[] args) {
 		boolean debug = args.length > 0 && args[0].equals("debug");
 
-		Level level1 = new Level1(debug);
-		level1.solve();
+		Level level2 = new Level2(debug);
+		level2.solve();
 	}
 }
--- src/at/htlleonding/diegaertner/cc2023/levels/Level2.java
+++ src/at/htlleonding/diegaertner/cc2023/levels/Level2.java
@@ -2,7 +2,7 @@ package at.htlleonding.diegaertner.cc2023.levels;
 
 import at.htlleonding.diegaertner.cc2023.Level;
 
-import java.util.List;
+import java.util.*;
 
 public class Level2 extends Level {
     public Level2(boolean debug) {
@@ -12,6 +12,60 @@ public class Level2 extends Level {
 
     @Override
     protected Object solve(List<String> input, int subLevel) {
-        return null;
+
+        int n = Integer.parseInt(input.get(0));
+
+        input = input.subList(1, input.size());
+
+        Set<String> uniqueLines = new HashSet<>();
+        Map<String, Integer> lineCount = new HashMap<>();
+
+        for (String line : input) {
+            Set<String> rotations = new HashSet<>();
+            String[] chars = line.split(",");
+
+            // Erzeugen von Rotationsvarianten der Zeile
+            for (int i = 0; i < chars.length; i++) {
+                StringBuilder rotation = new StringBuilder();
+                for (int j = 0; j < chars.length; j++) {
+                    if (j != chars.length -1) {
+                        rotation.append(chars[(i + j) % chars.length]).append(",");
+                    } else {
+                        rotation.append(chars[(i + j) % chars.length]);
+                    }
+                }
+                rotations.add(rotation.toString());
+            }
+
+            // Überprüfen, ob eine der Rotationen bereits in den einzigartigen Zeilen ist
+            boolean isNew = true;
+            String uniqueLine = "";
+
+            for (String rotation : rotations) {
+                if (uniqueLines.contains(rotation)) {
+                    isNew = false;
+                    uniqueLine = rotation;
+                    break;
+                }
+            }
+
+            // Wenn es eine neue Zeile ist, füge sie zu den einzigartigen Zeilen hinzu
+            if (isNew) {
+                for (String rotation : rotations) {
+                    uniqueLines.add(rotation);
+                }
+            } else {
+                lineCount.put(uniqueLine, lineCount.getOrDefault(uniqueLine, 1) + 1);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : lineCount.entrySet()) {
+            result.append(entry.getValue()).append(" ").append(entry.getKey()).append("\n");
+        }
+
+        // Ausgabe der Zeilen mit Anzahl
+        System.out.println(result.toString());
+        return result.toString();
     }
 }
