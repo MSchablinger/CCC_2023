fd38cef7035740c31a9b4ee7438b1156111b0791
mschablinger <m.schablinger@students.htl-leonding.ac.at>
2023-10-20 09:12:56 +0200

    removed past files

--- src/at/htlleonding/diegaertner/cc2023/Main.java
+++ src/at/htlleonding/diegaertner/cc2023/Main.java
@@ -1,7 +1,5 @@
 package at.htlleonding.diegaertner.cc2023;
 
-import at.htlleonding.diegaertner.cc2023.levels.Level4;
-
 public class Main {
 	public static void main(String[] args) {
 		boolean debug = args.length > 0 && args[0].equals("debug");
